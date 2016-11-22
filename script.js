e9a4e792ad041c07feaf47628a50bd0f7ffb6d43
zobos <zgovorovski@gmail.com>
2016-11-22 17:59:42 +0000

    Added Essential Core Files

---
+++ script.js
@@ -0,0 +1,13 @@
+var penColour = 'black';
+
+function setPixelColor(pixel) {
+  pixel.style.backgroundColor = penColour;
+}
+
+function setPenColour(pen) {
+ penColour = pen;
+}
+
+function clearCanvas() {
+  location.reload();
+}
